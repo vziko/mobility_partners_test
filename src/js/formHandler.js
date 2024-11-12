b11086d0e059e3a8e3e3e21fd039fb65071a81a6
Vladyslav Olkhovskyi <vzikooo@gmail.com>
2024-11-12 09:58:41 +0200

    Added normal validation for form

---
+++ src/js/formHandler.js
@@ -0,0 +1,126 @@
+function FormHandler(form, success) {
+	this.form = form;
+	this.inputLists = [];
+	this.validators = {};
+
+	this.init = () => {
+		this.form.setAttribute('novalidate', true);
+		this.createInputsList();
+		this.events();
+	}
+
+	this.createInputsList = () => {
+		Array.from(this.form.elements).forEach(input => {
+			if (input.tagName.toLowerCase() === 'input' && input.name) {
+
+				const errorMessage = document.createElement('div');
+				errorMessage.classList.add('error-message');
+
+				this.inputLists.push({
+					input,
+					isRequired: input.required,
+					validationType: input.dataset.validationType,
+					isValid: !(input.required || input.dataset.validationType),
+					errorElement: errorMessage,
+				});
+			}
+		});
+	}
+
+	this.events = () => {
+		this.form.addEventListener('submit', this.submit.bind(this));
+	}
+
+	this.submit = e => {
+		e.preventDefault();
+		e.stopPropagation();
+
+		let correctInputCounter = 0;
+
+		this.inputLists.forEach((element) => {
+			console.log(element.isRequired);
+
+			if(element.validationType) {
+				this.checkValidationType(element) && correctInputCounter++;
+			} else if(element.isRequired) {
+				this.validators.required(element) && correctInputCounter++;
+			} else {
+				correctInputCounter++
+			}
+		})
+
+		if (correctInputCounter === this.inputLists.length) {
+			this.successValidation();
+		}
+	}
+
+	this.checkValidationType = element => {
+		switch (element.validationType) {
+			case 'email':
+				return this.validators.email(element);
+			case 'phone':
+				return this.validators.phone(element);
+			default: return false
+		}
+	}
+
+	this.validators.required = element => {
+		const isValid = element.input.value.trim() !== '';
+		return this.checkError(element, isValid, 'This field is required.');
+	}
+
+	this.validators.email = element => {
+		if (this.validators.required(element)) {
+			const isValid = String(element.input.value)
+				.toLowerCase()
+				.match(
+					/^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|.(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
+				);
+			return this.checkError(element, isValid, 'Please enter a valid email.');
+		} else {
+			return false;
+		}
+	}
+
+	this.validators.phone = element => {
+		if (this.validators.required(element)) {
+			const isValid = element.input.value.length === 13; // Todo Switch to proper validation
+			return this.checkError(element, isValid, 'Please enter a valid phone number.');
+		} else {
+			return false;
+		}
+	}
+
+	this.checkError = (element, isValid, errorMessage) => {
+		if(!isValid) {
+			this.addError(element, errorMessage);
+			return false;
+		} else {
+			this.removeError(element);
+			return true;
+		}
+	}
+
+	this.addError = (element, errorMessage) => {
+		element.input.classList.add('is-invalid');
+		element.errorElement.textContent = errorMessage;
+		element.input.after(element.errorElement)
+	}
+
+	this.removeError = element => {
+		element.input.classList.remove('is-invalid');
+		element.errorElement.remove();
+	}
+
+	this.successValidation = () => {
+		this.onAfterSubmit();
+	}
+
+	this.afterSubmit = callback => {
+		this.onAfterSubmit = callback;
+	}
+
+	this.init();
+}
+
+export { FormHandler };
--- src/js/main.js
+++ src/js/main.js
@@ -1,48 +1,16 @@
 import { Dropdown } from 'bootstrap'
+import { FormHandler } from './formHandler'
 
-const TakeIdeaForm = document.querySelector('.take-idea-form');
-const formEmail = TakeIdeaForm.querySelector('input[name="email"]');
+const $takeIdeaForm = document.querySelector('[data-form="pre-registration"]');
 
-TakeIdeaForm.addEventListener('submit', (e) => {
-	e.preventDefault();
-	e.stopPropagation();
+const formHandler = new FormHandler($takeIdeaForm);
 
-	let errorMessage = document.querySelector('.error-message');
-
-	if (!errorMessage) {
-		errorMessage = document.createElement('div');
-		errorMessage.classList.add('error-message');
-		errorMessage.textContent = 'Invalid email';
-	}
-
-	if(validateEmail(formEmail.value)) {
-		formEmail.classList.remove('is-invalid');
-		if (errorMessage) {
-			errorMessage.remove();
-		}
-
-		success();
-	} else {
-		if (!document.contains(errorMessage)) {
-			formEmail.after(errorMessage);
-		}
-		formEmail.classList.add('is-invalid');
-	}
-})
-
-const success = () => {
+formHandler.afterSubmit(() => {
 	const successMessage = document.createElement('div');
+
 	successMessage.classList.add('success-message');
 	successMessage.textContent = 'Thank You. Check You Email';
-	TakeIdeaForm.after(successMessage);
-	TakeIdeaForm.style.display = 'none';
-}
 
-
-const validateEmail = (email) => {
-	return String(email)
-		.toLowerCase()
-		.match(
-			/^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|.(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
-		);
-};
+	$takeIdeaForm.after(successMessage);
+	$takeIdeaForm.style.display = 'none';
+});
